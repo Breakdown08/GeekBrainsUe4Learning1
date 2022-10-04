0c23fe59dfc06d176c540ec5207ea776c12f154e
Breakdown08 <41376576+Breakdown08@users.noreply.github.com>
2022-10-05 02:14:14 +0700

    Update main.cpp

--- OOPLesson8/main.cpp
+++ OOPLesson8/main.cpp
@@ -1,89 +1,207 @@
 #include <iostream>
-
-class DivisionByZero
-{
-private:
-    std::string m_error;
-public:
-    DivisionByZero() : m_error("second argument can not be ZERO") { }
-    const char* getError() 
-    { 
-        return m_error.c_str(); 
-    }
-};
-
+#include <clocale>  
+#include <optional>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream> // для stringstream
+#include <iomanip>
+#include <algorithm>
 
 template < typename T >
-T Div(T& a, T& b)
+int GetLongestString(std::string type, T entity)
 {
-    if (b == 0)
+    if (type == "lastname")
     {
-        throw DivisionByZero();
-    };
-    return a / b;
+        entity.storage.sort();
+    }
+    else if (type == "firstname")
+    {
+
+    }
+    else if (type == "patronymic")
+    {
+
+    }
+}
+
+struct Person
+{
+    std::string LastName;
+    std::string FirstName;
+    std::optional<std::string> Patronymic;
+};
+std::ostream& operator << (std::ostream& os, const Person& p)
+{
+    //в setw аргумент подложить длину строки самой длинной фамилии, также имени и отчества, должно заработать, вызвать функцией
+    os << std::setw(20) << p.LastName << " " << p.FirstName;
+    if (p.Patronymic)
+    {
+        os << " " << *p.Patronymic;
+    }
+    return os;
+}
+
+bool operator < (const Person& p1, const Person& p2)
+{
+    return tie(p1.LastName, p1.FirstName, p1.Patronymic) <
+        tie(p2.LastName, p2.FirstName, p2.Patronymic);
+}
+
+bool operator == (const Person& p1, const Person& p2)
+{
+    return tie(p1.LastName, p1.FirstName, p1.Patronymic) ==
+        tie(p2.LastName, p2.FirstName, p2.Patronymic);
+}
+
+struct PhoneNumber
+{
+    int state;
+    int city;
+    std::string number;
+    std::optional<int> additional;
+};
+
+std::ostream& operator << (std::ostream& os, const PhoneNumber& n)
+{
+    os << "+" << n.state << "(" << n.city << ")" << n.number;
+    if (n.additional)
+    {
+        os << " " << *n.additional;
+    }
+    return os;
+}
+
+bool operator < (const PhoneNumber& n1, const PhoneNumber& n2)
+{
+    return tie(n1.state, n1.city, n1.number, n1.additional) <
+        tie(n2.state, n2.city, n2.number, n2.additional);
+}
+
+bool operator == (const PhoneNumber& n1, const PhoneNumber& n2)
+{
+    return tie(n1.state, n1.city, n1.number, n1.additional) ==
+        tie(n2.state, n2.city, n2.number, n2.additional);
 }
 
 
-class Ex
-{  
-public:
-    double x;
-    Ex(double x) : x(x) { };
-};
+const std::string DATA_FILE = "data.txt";
 
-class Bar
+class PhoneBook
 {
-private:
-    double y;
 public:
-    Bar()
-    {
-        y = 0;
-    }
-    void Set(double a)
-    {
-        if ((y + a) > 100)
-        {
-            throw Ex(a * y);
-        }
-        else 
-        {
-            y = a;
-        }
-    }
-};
+    std::vector<std::pair<Person, PhoneNumber>> storage;
 
-int main()
+    PhoneBook(std::ifstream& stream)
+    {
+        
+        if (stream.is_open())
+        {
+            std::string row;
+            std::stringstream rowStream;
+            std::string personString;
+            std::string personPatronymic;
+            std::string phoneState;
+            std::string phoneCity;
+            std::string phoneNumber;
+            std::string phoneAdditional;
+            Person person;
+            PhoneNumber phone;
+            std::pair<Person, PhoneNumber> pair;
+            while (!stream.eof())
+            {
+                std::getline(stream, row);
+                rowStream << row;
+                std::getline(rowStream, personString, ';') >> phoneState >> phoneCity >> phoneNumber >> phoneAdditional;
+
+                phone.state = std::stoi(phoneState);
+                phone.city = std::stoi(phoneCity);
+                phone.number = phoneNumber;
+                if (phoneAdditional != "")
+                {
+                    phone.additional = std::stoi(phoneAdditional);
+                }
+                rowStream.clear();
+                rowStream << personString;
+                personPatronymic = "";
+                person.FirstName = "";
+                person.LastName = "";
+                person.Patronymic.reset();
+                rowStream >> person.LastName >> person.FirstName >> personPatronymic;
+                if (personPatronymic != "")
+                {
+                    person.Patronymic = personPatronymic;
+                }
+                rowStream.clear();
+                pair.first = person;
+                pair.second = phone;
+                storage.push_back(pair);
+            }
+            stream.close();
+        }
+    }
+
+    void SortByPhone();
+    void SortByName()
+    {
+        std::sort(storage.begin(), storage.end());
+    }
+
+
+    friend std::ostream& operator<< (std::ostream& out, const PhoneBook& phoneBook);
+    
+};
+std::ostream& operator<< (std::ostream& out, PhoneBook& phoneBook)
 {
-    //------------------------------1-------------------------------
-    int a, b;
-    a = 18;
-    b = 0;
-    try
+    for (size_t i = 0; i < phoneBook.storage.size(); i++)
     {
-        std::cout << Div(a, b) << std::endl;
+        out << phoneBook.storage[i].first << " " << phoneBook.storage[i].second << std::endl;
     }
-    catch (DivisionByZero&  exception)
-    {
-        std::cerr << "Error: " << exception.getError() << std::endl;
-    }
-    //------------------------------2-------------------------------
-    std::cout << std::endl;
-    Bar bar;
-    int arg = 1;
-    std::cout << "START TESTING" << std::endl;
-    while (arg != 0)
-    {
-        std::cout << "input arg: " << std::endl;
-        std::cin >> arg;
-        try
-        {
-            bar.Set(arg);
-        }
-        catch (Ex exception)
-        {
-            std::cerr << "Error: " << exception.x << std::endl;
-        }
-    }
-    return 0;
+    return out;
+}
+
+//int main()
+//{
+//  setlocale(LC_ALL, "Russian");
+//  Person p;
+//  p.FirstName = "Кирилл";
+//  p.LastName = "Мищенко";
+//  p.Patronymic = "Александрович";
+//  std::cout << p << std::endl;
+//}
+
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    std::ifstream file(DATA_FILE); 
+    PhoneBook book(file);
+    std::cout << book;
+    //std::cout << "------SortByPhone-------" << std::endl;
+    ////book.SortByPhone();
+    //std::cout << book;
+    //std::cout << "------SortByName--------" << std::endl;
+    book.SortByName();
+    std::cout << book;
+    //std::cout << "-----GetPhoneNumber-----" << std::endl;
+    // лямбда функция, которая принимает фамилию и выводит номер телефона
+    //человека, либо строку с ошибкой
+    //  auto print_phone_number = [&book](const std::string& surname) {
+    //      std::cout << surname << "\t";
+    //  auto answer = book.GetPhoneNumber(surname);
+    //  if (get<0>(answer).empty())
+    //      std::cout << get<1>(answer);
+    //  else
+    //      std::cout << get<0>(answer);
+    //  std::cout << std::endl;
+    //};
+    // вызовы лямбды
+    //print_phone_number("Ivanov");
+    //print_phone_number("Petrov");
+    //std::cout << "----ChangePhoneNumber----" << std::endl;
+    //book.ChangePhoneNumber(Person{ "Kotov", "Vasilii", "Eliseevich" },
+    //  PhoneNumber{ 7, 123, "15344458", std::nullopt });
+    //book.ChangePhoneNumber(Person{ "Mironova", "Margarita", "Vladimirovna" },
+    //  PhoneNumber{ 16, 465, "9155448", 13 });
+    //std::cout << book;
 }
